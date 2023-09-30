1138022b9e0b077f231b0ec12b7d35accf8d9622
Tosiek <antonizieciak@gmail.com>
2023-10-01 01:44:53 +0200

    question ch00sing

--- src/engine.py
+++ src/engine.py
@@ -1,4 +1,7 @@
 import numpy as np
+import math
+import copy
+from functools import cmp_to_key
 from . import data
 
 fields, questions = data.from_csv('data.csv')
@@ -11,19 +14,52 @@ def run():
     print("To jest proof of concept. Odpowiadaj liczbami w [0, 1]")
 
     while not stop_condition(available_questions):
-        question_id = next_question(available_questions)
+        question_id = next_question(questions_answered, answers, available_questions)
         questions_answered.append(question_id)
         answer = input(f"{questions[question_id]}: ")
         answers.append(float(answer))
 
         probs = calculate_probabilites(questions_so_far=questions, answers_so_far=answers)
-        print(probs)
+        print(sorted(probs, key=cmp_to_key(compare)))
+
+def compare(item1, item2):
+    if item1['probability'] > item2['probability']:
+        return -1
+    elif item1['probability'] < item2['probability']:
+        return 1
+    else:
+        return 0
 
 def stop_condition(questions):
     return len(questions) == 0
 
-def next_question(available) -> int:
-    return available.pop()
+def next_question(questions_so_far, answers_so_far, quezdionz) -> int:
+    next_entropies = []
+    for quezdion in quezdionz:
+        new_questions = copy.deepcopy(questions_so_far)
+        new_questions.append(quezdion)
+        new_answers = copy.deepcopy(answers_so_far)
+        new_answers.append(1)
+        next_probabilities = calculate_probabilites(new_questions, new_answers)
+        next_entropies.append((entropy(next_probabilities), quezdion))
+    min_entropy = next_entropies[0][0]
+    question = next_entropies[0][1]
+    for etr in next_entropies:
+        if etr[0] < min_entropy:
+            min_entropy = etr[0]
+            question = etr[1]
+
+    quezdionz.remove(question)
+
+    return question
+
+
+def entropy(probabilities):
+    e = 0
+    for pi in probabilities:
+        e += pi['probability'] * math.log(pi['probability'], 2)
+
+    return e
 
 def calculate_probabilites(questions_so_far, answers_so_far):
     probabilities = []
